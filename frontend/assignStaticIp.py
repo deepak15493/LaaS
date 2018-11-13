6a5f24f1c07a4b732de405c9a69ec7fd27d0d86b
tushar <tvnargun@ncsu.edu>
2018-11-13 03:38:51 -0500

    changed assignStaticIp.py

--- frontend/assignStaticIp.py
+++ frontend/assignStaticIp.py
@@ -1,44 +1,44 @@
-
-import  os
+import os
 import paramiko
 import csv
 import libvirt
 import time
+import sys
 from collections import defaultdict
 
 
-hypervisorIP=""
-username=""
-password=""
 listOfLBs=[]
 listOfIPOfLBs=[]
 
 def assignStaticIPOnHypervisor1():
-	global listOfLBs, username, password , hypervisorIP
+	global listOfLBs,listOfIPOfLBs
 	last_octet = 0 ;
 		
 	for LBIP in listOfIPOfLBs:
-		ssh = getSshInstanceFromParamiko(LBIP , username , password);	
-    		command_to_assign_static_ip_eth0 = 'ip addr add 192.168.10.' + last_octet + '/24 dev eth0'
-    		command_to_assign_static_ip_eth1 = 'ip addr add 192.168.20.' + last_octet + '/24 dev eth1'
+		ssh = getSshInstanceFromParamiko(LBIP , "root" , "tushar123");	
+    		command_to_assign_static_ip_eth0 = 'ip addr add 192.168.10.' + str(last_octet) + '/24 dev eth0'
+    		command_to_assign_static_ip_eth1 = 'ip addr add 192.168.20.' + str(last_octet) + '/24 dev eth1'
     		ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command_to_assign_static_ip_eth0)
     		print(ssh_stdout.readlines())
     		ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command_to_assign_static_ip_eth1)
     		print(ssh_stdout.readlines())
+		last_octet += 1
 	ssh.close()
 
 def assignStaticIPOnHypervisor2():
-	global listOfLBs, username, password , hypervisorIP
+	global listOfLBs,listOfIPOfLBs
 	last_octet = 10 ;
 		
 	for LBIP in listOfIPOfLBs:
-		ssh = getSshInstanceFromParamiko(LBIP , username , password);	
-    		command_to_assign_static_ip_eth0 = 'ip addr add 192.168.10.' + last_octet + '/24 dev eth0'
-    		command_to_assign_static_ip_eth1 = 'ip addr add 192.168.20.' + last_octet + '/24 dev eth1'
+		print " LBIP ", LBIP
+		ssh = getSshInstanceFromParamiko(LBIP , "root" , "tushar123");	
+    		command_to_assign_static_ip_eth0 = 'ip addr add 192.168.10.' + str(last_octet) + '/24 dev eth0'
+    		command_to_assign_static_ip_eth1 = 'ip addr add 192.168.20.' + str(last_octet) + '/24 dev eth1'
     		ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command_to_assign_static_ip_eth0)
     		print(ssh_stdout.readlines())
     		ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command_to_assign_static_ip_eth1)
     		print(ssh_stdout.readlines())
+		last_octet += 1
 	ssh.close()
 
 
@@ -46,23 +46,19 @@ def getSshInstanceFromParamiko(ipaddress, username, password):
     ssh = paramiko.SSHClient()
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(ipaddress, port=22, username=username, password=password)
+    ssh.connect(ipaddress, port=22, username="root", password="tushar123")
     return ssh
 
-if __name__ == "__main__":
-	global listOfLBs, username, password , hypervisorIP
-	hypervisorIP = sys.argv[1]
-	username = sys.argv[2]
-	password = sys.argv[3]
-	arrOfLBs = (sys.argv[4]).strip().split(",")
-	arrIPOfLBs = (sys.argv[5]).strip().split(",")
-	hypervisorFlag = sys.argv[6].strip()
+if _name_ == "_main_":
+	arrOfLBs = (sys.argv[1]).strip().split(",")
+	arrIPOfLBs = (sys.argv[2]).strip().split(",")
+	hypervisorFlag = sys.argv[3].strip()
 	
 	for LB in arrOfLBs:
 		listOfLBs.append(LB)
 	
 	for IP in arrIPOfLBs:
-		listOfIPOfLBs.append(LB)
+		listOfIPOfLBs.append(IP)
 	
 	if hypervisorFlag == str(1):
 		assignStaticIPOnHypervisor1()
